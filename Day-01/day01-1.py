60a4f9a0ffadc0b2676750939971a77fc766901d
bradley-gee <bradley.gee@informed.com>
2019-12-04 09:51:28 +0000

    Day Uno

--- Day-01/day01-1.py
+++ Day-01/day01-1.py
@@ -1,5 +1,13 @@
 def main(puzzle_input):
-    # Parse the input
-    # Solve the puzzle
+
+    puzzle_input = puzzle_input.splitlines()
+    solution = 0
+    for val in puzzle_input:
+        solution += ((int(val) / 3) - 2)
 
     return str(solution)
+
+
+with open("input.txt","r") as input_file:
+    puzzle_input = input_file.read()
+    print (main(puzzle_input))
--- Day-01/day01-2.py
+++ Day-01/day01-2.py
@@ -1,5 +1,22 @@
 def main(puzzle_input):
-    # Parse the input
-    # Solve the puzzle
 
+    puzzle_input = puzzle_input.splitlines()
+    solution = 0
+    for val in puzzle_input:
+        solution += get_fuel_for_mass(int(val))
     return str(solution)
+
+def get_fuel_for_mass(added_mass):
+    fuel_for_added_mass = (added_mass / 3) - 2
+    if fuel_for_added_mass > 0:
+        fuel_for_added_mass += get_fuel_for_mass(fuel_for_added_mass)
+    else:
+        return 0
+
+    return fuel_for_added_mass
+
+
+
+with open("input.txt","r") as input_file:
+    puzzle_input = input_file.read()
+    print (main(puzzle_input))
